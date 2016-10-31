7b1c6b5a8d9ddbd21e5319bdfb53686728d0328d
Caio e Rodrigo <caiohenriquemaciel@gmail.com>
2016-10-31 21:51:16 -0200

    Usuário Cadastra Vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,15 @@
 class JobsController < ApplicationController
   def show
-    @job = Job.find(params[:id]) 
+    @job = Job.find(params[:id])
+  end
+  def new
+    @job = Job.new
+  end
+  def create
+    @job = Job.create(job_params)
+    redirect_to job_url(@job)
+  end
+  def job_params
+      params.require(:job).permit(:title, :location, :category, :company, :description)
   end
 end
