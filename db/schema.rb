4e9a5255c8f61d2802e379f95477d875915a33ef
Caio e Rodrigo <caiohenriquemaciel@gmail.com>
2016-11-01 20:08:25 -0200

    Usuário marca vags em destaque

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027223056) do
+ActiveRecord::Schema.define(version: 20161101213710) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161027223056) do
     t.string   "location"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "featured"
   end
 
 end
