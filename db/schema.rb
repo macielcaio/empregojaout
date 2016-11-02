6b7beaa8155df9ecb5a5041f80474bbf57421bc9
Caio e Rodrigo <caiohenriquemaciel@gmail.com>
2016-11-01 22:16:01 -0200

    Usuário usa empresas cadastradas

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101213710) do
+ActiveRecord::Schema.define(version: 20161102001411) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "mail"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "category"
-    t.string   "company"
     t.text     "description"
     t.string   "location"
     t.datetime "created_at",  null: false
