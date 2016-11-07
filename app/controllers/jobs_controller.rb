35794b6182fa8604c5fbdaab8066ca2547cf3475
Caio e Rodrigo <caiohenriquemaciel@gmail.com>
2016-11-07 20:50:48 -0200

    Usuário edita vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,6 +17,21 @@ class JobsController < ApplicationController
     end
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to job_url(@job)
+    else
+      flash[:alert] = 'Não foi possível atualizar a vaga'
+      render :edit
+    end
+
+  end
+
   def job_params
       params.require(:job).permit(:title, :location, :category, :company_id, :description, :featured)
   end
