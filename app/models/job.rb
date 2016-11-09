964de72f6ba38f86ceb0c2a926d3fa9b547ed9de
Caio e Rodrigo <caiohenriquemaciel@gmail.com>
2016-11-09 18:57:51 -0200

    Usuário usa categorias cadastradas

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
   belongs_to :company
-  validates :title, :category, :location, :description, presence: true
+  belongs_to :category
+  validates :title, :location, :description, presence: true
 end
